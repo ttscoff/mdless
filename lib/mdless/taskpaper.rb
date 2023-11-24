2bc919fdd63c62c82e80fbe4c6df1f8801e4d3dd
Brett Terpstra <me@brettterpstra.com>
2023-11-24 15:13:27 -0600

    fix project regex
    - FIXED: Project regex matching `- PROJECT NAME:`

--- lib/mdless/taskpaper.rb
+++ lib/mdless/taskpaper.rb
@@ -2,8 +2,8 @@
 
 module CLIMarkdown
   module TaskPaper
-    TASK_RX = /^(?<indent>(?:    |\t)*)(?<marker>-)(?<task>\s+\S.*?)$/
-    PROJECT_RX = /^(?<indent>(?:    |\t)*)(?<project>[^-]\S.*?:)(?<tags> @\S+)*$/
+    TASK_RX = /^(?<indent>(?:    |\t)*?)(?<marker>-)(?<task>\s+\S.*?)$/
+    PROJECT_RX = /^(?<indent>(?:    |\t)*?)(?<project>[^- \t].*?:)(?<tags> @\S+)*$/
     NOTE_RX = /^(?<indent>(?:    |\t)+)(?<note>(?<!- ).*?(?!:))$/
 
     class << self
@@ -37,10 +37,13 @@ module CLIMarkdown
       end
 
       def is_taskpaper?(input)
+        pp input.to_enum(:scan, PROJECT_RX).map { Regexp.last_match }
         projects = input.split(PROJECT_RX)
         tasks = 0
-        projects.each do |proj|
-          tasks += proj.scan(TASK_RX).count
+        if projects.count > 1
+          projects.each do |proj|
+            tasks += proj.scan(TASK_RX).count
+          end
         end
 
         tasks >= 6
