962c36f4608af82e92ebf29b80f59beb387665dd
Brett Terpstra <brett.terpstra@oracle.com>
2025-05-05 07:54:57 -0500

    fix for longest_element in invalid markup
    @fixed longest_element in mismatched code fences

--- lib/mdless/array.rb
+++ lib/mdless/array.rb
@@ -2,10 +2,14 @@
 
 class ::Array
   def longest_element
+    return self if nil? || empty?
+
     group_by(&:size).max.last[0]
   end
 
   def longest_elements
+    return [] if nil? || empty?
+
     group_by(&:size).max.last
   end
 end
